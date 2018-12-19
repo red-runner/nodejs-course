03f45893ffc600051e64389e612bea22282540eb
red--runner <pol@lodgify.com>
2018-12-19 23:42:19 +0100

    Basic starter files

---
+++ index.js
@@ -0,0 +1,15 @@
+var rect = require('./rectangle')
+
+function solveRect(l, b) {
+    console.log("Solving rectangle with l = " + l + "and b = " + b)
+
+    if(l <= 0 || b <= 0) {
+        console.log("Rectangle dimensions should be greater than zero")
+    } else {
+        console.log("The area of the rectangle is " + rect.area(l,b))
+        console.log("The perimeter of the rectangle is " + rect.perimeter(l,b))
+    }
+}
+
+solveRect(2,4);
+solveRect(3,5);
